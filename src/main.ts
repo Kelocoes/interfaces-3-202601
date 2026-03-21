63a64e184f48835e94036874b6089f5f53f20703
kelocoes <kevin.rodriguez109@gmail.com>
2026-03-21 17:10:46 -0500

    Cors, docker!

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    app.enableCors();
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((error) => {
