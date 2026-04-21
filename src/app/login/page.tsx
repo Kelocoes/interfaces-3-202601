09efbec00e07178196f2687c1b4b9502568ede17
Kelocoes <kevin.rodriguez109@gmail.com>
2026-04-21 08:11:36 -0500

    Commit!

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+export default function Login() {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Login</h1>
+                    <p className="py-6">
+                        Please enter your credentials to access your account.
+                    </p>
+                    <button className="btn btn-primary">Login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/common/components/CustomBadge.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+import { useState } from 'react';
+
+type CustomBadgeProps = {
+    badgeContent: number;
+    color: "primary" | "secondary" | "default" | "error" | "info" | "success" | "warning";
+}
+
+export default function CustomBadge({ badgeContent, color }: CustomBadgeProps) {
+    const [counter, setCounter] = useState<number>(badgeContent);
+    return (
+        <Badge badgeContent={counter} color={color}>
+            <MailIcon color="primary" onClick={() => setCounter(counter + 1)} />
+        </Badge>
+    );
+}
